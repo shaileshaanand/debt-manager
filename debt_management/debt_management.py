b277a78419c1d9b0f432d0f13351de225363e2dd
shaileshaanand <anaandshailu@gmail.com>
2019-10-11 08:39:01 +0530

    version 0.0.8

--- debt_management/debt_management.py
+++ debt_management/debt_management.py
@@ -14,7 +14,7 @@ from .about_us import Ui_Dialog as About_Ui_Dialog
 Person = namedtuple('Person', ['name', 'personid', 'total'])
 Transaction = namedtuple(
     'Transaction', ['id', 'personid', 'desc', 'amount', 'date'])
-version = "0.0.7"
+version = "0.0.8"
 
 
 class PersonListWidgetItem(QListWidgetItem):
